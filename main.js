11bd8110e7a63d5b0ab9b7e464e8d9b01dcb392a
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-05-22 16:09:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,20 +8,18 @@ var startPlaying = function() {
 var start = function() {
 	opn("http://diep.io/").then(function() {
 		// maximise window, because if we dont, problems arise
-		switch(os.type()) {
-			case "Linux":
-				robot.keyTap("alt", "f10");
-				break;
-			case "Darwin":
-				robot.keyTap("f", ["command", ""control"]);
-				break;
-			case "Windows_NT":
-				robot.keyTap("up", "command");
-				break;
-			default:
-				// assume linux
-				robot.keyTap("alt", "f10");
+		var ostype = os.type();
+		if (ostype === "Linux") {
+			robot.keyTap("alt", "f10");
+		} else if (ostype === "Darwin") {
+			robot.keyTap("f", ["command", "control"]);
+		} else if (ostype === "Windows_NT") {
+			robot.keyTap("up", "command");
+		} else {
+			// assume its some really obscure linux distribution
+			robot.keyTap("alt", "f10");
 		}
+		
 		var screensize = robot.getScreenSize();
 		robot.moveMouse(Math.round(screensize.width), Math.round(screensize.height) - 10);
 		robot.mouseClick();
