05217f27edda8ea89eb08ae792b4107b4a1d27a4
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-05-23 10:06:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 var robot = require("robotjs");
 var opn = require("opn");
 
-var startPlaying = function() {
-	console.log("We are in");
+var g_v = {
+	centerx: Math.round(robot.getScreenSize().width / 2),
+	centery: Math.round(robot.getScreenSize().height / 2),
+	spinradius: robot.getScreenSize().height / 6
+};
+
+var g_f = {
+	getMouseLocForSpin: function(degree) {
+		function toRadians(angle) {
+  			return angle * (Math.PI / 180);
+		}
+		var x = g_v.centerx + g_v.spinradius * Math.cos(toRadians(degree));
+		var y = g_v.centery + g_v.spinradius * Math.sin(toRadians(degree));
+		return [x, y];
+	}
 };
 
 var start = function() {
@@ -20,12 +33,15 @@ var start = function() {
 			robot.keyTap("alt", "f10");
 		}
 		
-		var screensize = robot.getScreenSize();
-		robot.moveMouse(Math.round(screensize.width), Math.round(screensize.height) - 10);
+		robot.moveMouse(g_v.centerx, Math.round(g_v.centery * 1.1));
 		robot.mouseClick();
 		robot.keyTap("a", "control");
 		robot.typeString(process.argv[2] || "A BOTTY BOT");
 		robot.keyTap("enter");
 		startPlaying();
 	});
+};
+
+function startPlaying() {
+	console.log("We are in");
 };
