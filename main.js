46982c74798b1c2c14a55bbb232debd14917433d
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-05-29 12:29:38 -0400

    Update main.js

--- main.js
+++ main.js
@@ -22,13 +22,13 @@ var gameColors = {
     "255,232,106": "Summoner"
 };
 
-function toArrayBuffer(buf) {
+function toTypedArray(buf) {
     var ab = new ArrayBuffer(buf.length);
     var view = new Uint8Array(ab);
     for (var i = 0; i < buf.length; ++i) {
         view[i] = buf[i];
     }
-    return ab;
+    return view;
 }
 
 var g_v = {
@@ -210,7 +210,7 @@ function startPlaying() {
             var newWidth = Math.round(imagesize.width / resizefactor);
             var newHeight = Math.round(imagesize.height / resizefactor);
             sharp(imagebuffer).resize(newWidth, newHeight).raw().toBuffer().then(function(pixels) {
-                objecttracker.track(toArrayBuffer(pixels), newWidth, newHeight).then(function(recs) {
+                objecttracker.track(toTypedArray(pixels), newWidth, newHeight).then(function(recs) {
                     if (recs.length !== 0) {
                         recs.forEach(function(rec) {
                             var gameobject = gameColors[rec.color];
